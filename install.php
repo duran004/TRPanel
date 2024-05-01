892bf0de374b1def75a676eeefc77ccba483d5cc
Duran Can YILMAZ <duran4@rengaver.com>
2024-05-01 21:22:29 +0300

    git

--- install.php
+++ install.php
@@ -1,6 +1,10 @@
 <?php
-if (!shell_exec('git pull origin master')) {
-    die('Git update failed');
+$output = [];
+$return_var = 0;
+exec('git pull origin master 2>&1', $output, $return_var);
+if ($return_var !== 0) {
+    echo "Git update failed. Output: " . implode("\n", $output);
+    die();
 }
 
 echo phpinfo();
