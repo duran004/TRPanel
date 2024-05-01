e3aedabda5d20d798d310a4b68447c61934dc56b
Duran Can YILMAZ <duran4@rengaver.com>
2024-05-01 20:56:29 +0300

    otomatikleştirme

--- lamp.sh
+++ lamp.sh
@@ -8,20 +8,43 @@ if [ "$(id -u)" != "0" ]; then
   echo "Bu komut dosyasını çalıştırmak için root yetkilerine ihtiyacınız var."
   exit 1
 fi
-
+echo "### LAMP Kurulumu ###"
 # Paketleri güncelle
 apt update
 
 # Paketleri yükle
 apt install -y $packages
+echo "### LAMP Kurulumu Bitti ###"
 
-# MySQL ayarlarını yapılandır
-# Örnek olarak, root kullanıcısına parola belirleme
-# Bu adımı manuel olarak yapmak daha güvenlidir
-#mysql_secure_installation
+# MySQL için root parolası belirle
+echo "MySQL root parolası belirleyin:"
+read -s mysql_root_password
+echo "MySQL root parolasını tekrar girin:"
+read -s mysql_root_password_repeat
+
+# Parolaların eşleşip eşleşmediğini kontrol et
+if [ "$mysql_root_password" != "$mysql_root_password_repeat" ]; then
+  echo "Parolalar eşleşmiyor. Lütfen tekrar deneyin."
+  exit 1
+fi
+
+# MySQL root parolasını ayarla
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$mysql_root_password';"
+
+# PHP'nin MySQL eklentisini yükle
+apt install -y php-mysql php-curl php-gd php-intl php-json php-mbstring php-xml php-zip
+
+# Apache'nin mod_rewrite modülünü etkinleştir
+a2enmod rewrite
 
 # Servisleri başlat
 service apache2 start
 service mysql start
+echo "### Apache ve MySQL başlatıldı ###"
+
+# gitden projey çek /var/www/html dizinine
+cd /var/www/html
+git clone https://github.com/duran004/TRPanel.git
+echo "### Proje çekildi ###"
 
 echo "Kurulum tamamlandı!"
