0201ea536d391a0ff1f20429348986f84b18c909
Duran Can YILMAZ <duran4@rengaver.com>
2024-05-01 22:14:54 +0300

    v

--- update.php
+++ update.php
@@ -5,16 +5,17 @@ try {
     if ($my_version != $latest_version) {
         throw new Exception("New version available. Updating...");
     }
+    echo "<x-alert>You are up to date!</x-alert>";
 } catch (Exception $e) {
     echo $e->getMessage() . "<br>";
     $output = [];
     $return_var = 0;
     exec('git pull origin main 2>&1', $output, $return_var);
     if ($return_var !== 0) {
-        die("Git update failed. Output: " . implode("\n", $output));
+        echo "Git update failed. Output: " . implode("\n", $output);
     } else {
         echo "Git update successful. Output: " . implode("\n", $output);
-        header("Refresh:3");
-        exit;
     }
+    header("Refresh:5");
+    exit;
 }
