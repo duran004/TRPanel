b022c23b14a939f44e7a5644f331952d3a62f243
Duran Can YILMAZ <duran4@rengaver.com>
2024-05-02 04:24:37 +0300

    2024-05-02 04:24:37

--- app/View/View.php
+++ app/View/View.php
@@ -16,15 +16,16 @@ class View
 
             // Replace special template tags
             $output = str_replace("{{", "<?=", $output);
-            $output = str_replace("}}", "?>", $output);
-$output = str_replace("@foreach", "<?php foreach", $output);
-            $output = str_replace("):", "): ?>", $output);
-$output = str_replace("@endforeach", "<?php endforeach; ?>", $output);
+            $output = str_replace("@elseif", "<?php elseif", $output);
+            $output = str_replace("@foreach", "<?php foreach", $output);
+            $output = str_replace("):", '): ?>', $output);
+$output = str_replace("@endforeach", '<?php endforeach; ?>', $output);
 $output = str_replace("@if", "<?php if", $output);
-            $output = str_replace("@endif", "endif; ?>", $output);
+            $output = str_replace("}}", "?>", $output);
+$output = str_replace("@endif", "<?php endif; ?>", $output);
 $output = str_replace("@else", "<?php else: ?>", $output);
-$output = str_replace("@elseif", "<?php elseif", $output);
-            return eval("?>$output");
+
+return eval("?>$output");
 } else {
 throw new \Exception("View file '$view' not found");
 }
--- app/View/files.php
+++ app/View/files.php
@@ -1,9 +1,34 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
+    integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
 files view
 <x-files>
     <x-alert>File list:</x-alert>
-    <ul>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Icon</th>
+                <th>Name</th>
+                <th>Size</th>
+                <th>Last Modified</th>
+                <th>Type</th>
+                <th>Permissions</th>
+            </tr>
+        </thead>
         @foreach ($files as $file):
-        <li>{{ $file }}</li>
-        @endforeach
-    </ul>
+        <tr>
+            <td>
+                @if (is_dir($file)):
+                <img src="https://img.icons8.com/ios/50/000000/folder-invoices.png" />
+                @else
+                <img src="https://img.icons8.com/ios/50/000000/file.png" />
+                @endif
+            </td>
+            <td>{{ $file }}</td>
+            <td>{{ @filesize($file) }}</td>
+            <td>{{ @date('Y-m-d H:i:s', filemtime($file)) }}</td>
+            <td>{{ @filetype($file)=='dir' ? 'Directory' : 'File' }}</td>
+            <td>{{ @substr(sprintf('%o', fileperms($file)), -4) }}</td>
+            @endforeach
+    </table>
 </x-files>
