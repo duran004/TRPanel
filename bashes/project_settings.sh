d51f4942d9bf2970af9bd6c5e012d30756028308
Duran Can YILMAZ <duran4@rengaver.com>
2024-08-19 02:19:28 +0300

    fix

--- bashes/project_settings.sh
+++ bashes/project_settings.sh
@@ -17,15 +17,9 @@ echo -e "${GREEN}### Composer Kurulumu Bitti ###${NC}"
 
 # gitden projey çek /var/www/html dizinine
 cd /var/www/html
-echo -e "${YELLOW}### Proje çekiliyor ###${NC}"
-#varsa sil
-rm -rf TRPanel
-#gitden çek
-git clone https://github.com/duran004/TRPanel.git
 
-git config --global --add safe.directory /var/www/html/TRPanel
-echo -e "${GREEN}### Proje çekildi ###${NC}"
 # Proje dizinine yetki ver
+git config --global --add safe.directory /var/www/html/TRPanel
 chown -R www-data:www-data TRPanel
 chmod -R 755 TRPanel
 sudo chown www-data:www-data TRPanel/.htaccess
@@ -33,7 +27,7 @@ echo -e "${GREEN}### Yetki verildi ###${NC}"
 
 
 echo -e "${YELLOW}### Composer ile gerekli paketler yükleniyor ###${NC}"
-cd TRPanel
+cd /var/www/html/TRPanel
 composer install
 echo -e "${GREEN}### Gerekli paketler yüklendi ###${NC}"
 
