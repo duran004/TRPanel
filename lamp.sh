527209693c7441a59a642856d7c825d9e532be8e
Duran Can YILMAZ <duran4@rengaver.com>
2024-08-19 00:54:40 +0300

    git

--- lamp.sh
+++ lamp.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-# Debug: Çalıştırılan komutları ve değişkenleri yazdır
-set -x
 
 # Renk kodları
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-# Git yoksa yükle
-if ! dpkg -l | grep -q git; then
-  echo -e "${YELLOW}### Git yükleniyor ###${NC}"
-  sudo apt update
-  sudo apt install -y git
 
-  # Git'in kurulumunu kontrol et
-  if ! command -v git &> /dev/null; then
-      echo -e "${RED}Git yüklenemedi.${NC}"
-      exit 1
-  else
-      echo -e "${GREEN}Git yüklendi.${NC}"
-  fi
 
+# Git'in kurulumunu kontrol et
+if ! command -v git &> /dev/null; then
+    echo -e "${YELLOW}### Git yükleniyor ###${NC}"
+    sudo apt update
+    sudo apt install -y git
+    if ! command -v git &> /dev/null; then 
+        echo -e "${RED}Git yüklenemedi.${NC}"
+        exit 1
+    else
+        echo -e "${GREEN}Git yüklendi.${NC}"
+    fi
 else
-  echo -e "${GREEN}git zaten yüklü.${NC}"
+    echo -e "${GREEN}Git zaten yüklü.${NC}"
 fi
 
 # Dizin yoksa oluştur
