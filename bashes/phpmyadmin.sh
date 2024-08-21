967ee0e5ac71b198fb01206f3cea3402a6b20b73
Duran Can YILMAZ <duran4@rengaver.com>
2024-08-21 23:49:09 +0300

    pma fix

--- bashes/phpmyadmin.sh
+++ bashes/phpmyadmin.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
-echo -e "${GREEN}### phpmyadmin.sh ###${NC}"
+echo "### phpmyadmin.sh ###"
+
 # phpMyAdmin yükle
-echo -e "${YELLOW}### phpMyAdmin ${lang[installing]} ###${NC}"
+echo "### phpMyAdmin yükleniyor ###"
 sudo apt-get install phpmyadmin -y
+
 # phpMyAdmin yüklü mü kontrol et
 if [ ! -f /etc/phpmyadmin/config.inc.php ]; then
-  echo -e "${RED}### phpMyAdmin ${lang[not_installed]} ###${NC}"
+  echo "### phpMyAdmin yüklenemedi ###"
   exit 1
 else
-  echo -e "${GREEN}### phpMyAdmin ${lang[installed]} ###${NC}"
+  echo "### phpMyAdmin yüklendi ###"
 fi
+
 # phpMyAdmin'i Apache'e ekle
-echo -e "${YELLOW}### phpMyAdmin ${lang[adding_to_apache]} ###${NC}"
+echo "### phpMyAdmin Apache'e ekleniyor ###"
 sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
 sudo a2enconf phpmyadmin
+
 # Apache'yi yeniden başlat
-echo -e "${YELLOW}### Apache ${lang[restarting]} ###${NC}"
+echo "### Apache yeniden başlatılıyor ###"
 sudo service apache2 restart
+
 # phpMyAdmin'e erişim izni ver
-echo -e "${YELLOW}### phpMyAdmin ${lang[granting_access]} ###${NC}"
+echo "### phpMyAdmin erişim izni veriliyor ###"
 sudo sed -i "s/   Allow from None/    Allow from All/g" /etc/phpmyadmin/apache.conf
+
 # Apache'yi yeniden başlat
-echo -e "${YELLOW}### Apache ${lang[restarting]} ###${NC}"
+echo "### Apache yeniden başlatılıyor ###"
 sudo service apache2 restart
+
+# MySQL root kullanıcısının kimlik doğrulama eklentisini 'auth_socket'tan 'mysql_native_password'a değiştir
+echo "### MySQL root kimlik doğrulama yöntemi değiştiriliyor ###"
+sudo mysql -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'YeniSifre';
+FLUSH PRIVILEGES;
+EOF
+
+echo "### İşlem tamamlandı ###"
