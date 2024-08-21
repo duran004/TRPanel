b05652d48bc5e4a42ae2f9a1bd467e270bde5e32
Duran Can YILMAZ <duran4@rengaver.com>
2024-08-22 01:33:25 +0300

    route post

--- app/Controller/Route.php
+++ app/Controller/Route.php
@@ -4,31 +4,47 @@ namespace App\Controller;
 
 class Route
 {
-    public static $routes = [];
+    public static $routes = [
+        'GET' => [],
+        'POST' => []
+    ];
     public $request;
+    public $method;
 
     public static function get($route, $controllerMethod)
     {
-        self::$routes[$route] = $controllerMethod;
+        self::$routes['GET'][$route] = $controllerMethod;
     }
+
+    public static function post($route, $controllerMethod)
+    {
+        self::$routes['POST'][$route] = $controllerMethod;
+    }
+
     public function get_request()
     {
         return $this->request;
     }
 
-    public function __construct($request)
+    public function __construct($request, $method)
     {
         //if localhost 
         if (strpos($request, 'TRPanel') !== false) {
             $request = str_replace('TRPanel', '', $request);
         }
         $this->request = trim($request, '/');
+        $this->method = strtoupper($method);
     }
 
     public function run()
     {
-        if (array_key_exists($this->request, Route::$routes)) {
-            list($controller, $method) = explode('@', Route::$routes[$this->request]);
+        if (!in_array($this->method, ['GET', 'POST'])) {
+            http_response_code(405);
+            throw new \Exception("Invalid HTTP method");
+        }
+
+        if (array_key_exists($this->request, Route::$routes[$this->method])) {
+            list($controller, $method) = explode('@', Route::$routes[$this->method][$this->request]);
             $controller = new $controller;
             return $controller->$method();
         } else {
