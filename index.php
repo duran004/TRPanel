411fc5e7790ad282c998f216d2fe9031e906dd10
Duran Can YILMAZ <duran4@rengaver.com>
2024-08-22 01:45:54 +0300

    route fix

--- index.php
+++ index.php
@@ -6,8 +6,11 @@ use App\Controller\Route;
 use App\Helper\SysHelper;
 use App\Controller\AppController;
 
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-$route = new Route($_SERVER['REQUEST_URI']);
+
+$route = new Route($requestUri, $requestMethod);
 Route::get('', AppController::class . '@file_manager');
 Route::get('update', SysHelper::class . '@update');
 
