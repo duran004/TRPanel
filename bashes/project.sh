28b787b7910dbbd1f9c70a7e7afaab34d0af8441
Duran Can YILMAZ <duran4@rengaver.com>
2024-09-08 01:23:11 +0300

    2024-09-08 01:23:11

--- bashes/project.sh
+++ bashes/project.sh
@@ -46,7 +46,7 @@ cd /var/www/html/TRPanelLaravel
 #.env.example dosyasını .env olarak kopyala
 cp .env.example .env
 composer install
-npm run dev
+npm run build
 php artisan key:generate
 php artisan migrate
 php artisan db:seed
